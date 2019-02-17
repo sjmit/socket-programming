ed1e1ca2f10ab2ab913264f0cd8b76e03b4acf34
Sam Mitchell <sjmitchell96@gmail.com>
2019-02-16 18:02:57 -0600

    added eot

--- client.py
+++ client.py
@@ -7,29 +7,55 @@ import sys
 def main(argv):
 
     host = argv[0]
-    port = argv[1]
     filename = argv[2]
 
+    # Check if port is an integer
+    try:
+        port = int(argv[1])
+    except ValueError:
+        print("Port is not an integer")
+        return
+
+    # Open IPv4/TCP socket
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+
+        # Allows socket to be immediately reopened after closing
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.connect((host, port))
+
+        # Send arbitrary command
+        # This function will resend in the event of an error
         s.sendall(b'256')
+
+        # Recieve random port number
         r_port = int(s.recv(8).decode('utf8'))
 
+    # Socket connection implicitly closes
+
+    # Open IPv4/UDP socket
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+
+        # Allows socket to be immediately reopened after closing
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.connect((host, r_port))
+
+        # Open file
         try:
             f = open(filename, 'rb')
         except OSError:
             print("{} not found".format(filename))
         else:
             with f:
+                # Send packets of 4 until eof is reached
                 while True:
                     packet = f.read(4)
                     if not packet:
+                        s.send(b'\x04')  # EOT
                         break
                     s.sendall(packet)
+
+                    # Prints the server's response
+                    print(s.recv(4))
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -9,35 +9,56 @@ HOST = 'localhost'
 
 def main(argv):
 
-    port = sys.argv[0]
+    # Check if port is an integer
+    try:
+        port = int(argv[0])
+    except ValueError:
+        print("Port is not an integer")
+        return
 
+    # Open IPv4/TCP socket
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+
+        # Allows socket to be immediately reopened after closing
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind((HOST, port))
         s.listen()
+
+        # Blocking function call that waits for connection
         conn, _ = s.accept()
         with conn:
+
+            # Waits for correct message
             while True:
                 msg = conn.recv(4)
                 if msg == b'256':
                     break
+            # Generates random port in correct range
             r_port = random.randint(1024, 65535)
-            print("Negotiation has been detected. Please select your special random port", r_port)
-            conn.sendall(str(r_port)
+            print(("Negotiation has been detected. "
+                   "Please select your special random port"), r_port)
+
+            # Sends random port back to client
+            conn.sendall(str(r_port).encode('utf8'))
+
+    # Socket connection implicitly closes
 
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        
+        # Allows socket to be immediately reopened after closing
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        s.settimeout(0.1)
         s.bind((HOST, r_port))
+
+        # Opens file
         with open('output.txt', 'wb') as f:
             while True:
-                try:
-                    packet = s.recv(4)
-                except socket.timeout:
+                packet, address = s.recvfrom(4)
+                if packet == b'\x04':  # EOT
                     break
                 f.write(packet)
-                if len(packet) < 4:
-                    break
+
+                # Send ack back to client
+                s.sendto(packet.upper(), address)
 
 
 if __name__ == '__main__':
